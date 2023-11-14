39df7c61075d211af65167840cd91cb5137d5eb7
Rino <rinooktavianridwan@gmail.com>
2023-11-14 13:23:12 +0700

    hehe

---
+++ belajar-react/src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import BodyAtas from './BodyAtas';
+import Header from './Header'; // Sesuaikan dengan lokasi file Header.jsx
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <BodyAtas />
+      <h1>Selamat datang di Website React Saya!</h1>
+    </div>
+  );
+}
+
+export default App;
